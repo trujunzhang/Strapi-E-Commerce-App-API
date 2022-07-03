275831e60107424db38f830b0d4745c96d58ca53
djzhang <trujunzhang@gmail.com>
2022-07-03 16:44:48 +0800

    url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL", "http://23.105.205.158/"),
   app: {
     keys: env.array('APP_KEYS'),
   },
